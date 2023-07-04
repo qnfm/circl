b001199909e2ff18e9fbbbc29846eee9814ed89e
qnfm <uzinag@163.com>
2023-07-04 16:22:15 +0100

    mod tidy

--- kem/hybrid/hybrid.go
+++ kem/hybrid/hybrid.go
@@ -38,6 +38,7 @@ import (
 	"github.com/cloudflare/circl/kem/kyber/kyber1024"
 	"github.com/cloudflare/circl/kem/kyber/kyber512"
 	"github.com/cloudflare/circl/kem/kyber/kyber768"
+	"github.com/cloudflare/circl/kem/mceliece/mceliece8192128f"
 )
 
 var ErrUninitialized = errors.New("public or private key not initialized")
@@ -53,6 +54,7 @@ func Kyber768X448() kem.Scheme { return kyber768X4 }
 
 // Returns the hybrid KEM of Kyber1024Draft00 and X448.
 func Kyber1024X448() kem.Scheme { return kyber1024X }
+func Kyber1024M() kem.Scheme    { return kyber1024M }
 
 // Returns the hybrid KEM of Kyber768Draft00 and P-256.
 func P256Kyber768Draft00() kem.Scheme { return p256Kyber768Draft00 }
@@ -85,6 +87,12 @@ var kyber1024X kem.Scheme = &scheme{
 	"Kyber1024-X448",
 	x448Kem,
 	kyber1024.Scheme(),
+}
+
+var kyber1024M kem.Scheme = &scheme{
+	"Kyber1024-McEliece8192128f",
+	kyber1024.Scheme(),
+	mceliece8192128f.Scheme(),
 }
 
 // Public key of a hybrid KEM.
